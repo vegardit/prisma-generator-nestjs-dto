db4b4fb89737973f4fc7cee3d9fd7402fad6f4b3
Timo Gruber <bcofl292@eondata.onmicrosoft.com>
2021-07-30 13:48:02 +0200

    Update index-collection-helpers.ts

--- src/generator/index-collection-helpers.ts
+++ src/generator/index-collection-helpers.ts
@@ -1,4 +1,3 @@
-import { IndexCollection } from './types';
 import * as path from 'path';
 
 // Generate import names
@@ -10,33 +9,6 @@ const exportName = 'export * from';
 // Extension of files
 const ext = '.ts';
 
-interface GenerateIndexParam {
-  fileName: string;
-  indexCollections: IndexCollection[];
-}
-
-// Updates the index collection
-export const updateIndexCollection = ({
-  fileName,
-  indexCollections,
-}: GenerateIndexParam) => {
-  const current = indexCollections.findIndex(
-    ({ dir }) => dir === path.dirname(fileName),
-  );
-
-  const exportContent = `${exportName} ${createImportName(
-    path.basename(fileName).replace(ext, ''),
-  )}`;
-
-  const exists = current !== -1;
-
-  const updatedIndex = {
-    dir: exists ? indexCollections[current].dir : path.dirname(fileName),
-    content: exists
-      ? `${indexCollections[current].content}\n${exportContent}`
-      : exportContent,
-  };
-
-  if (exists) indexCollections[current] = updatedIndex;
-  else indexCollections.push(updatedIndex);
-};
+// Create an export statement for index.ts
+export const exportContent = (fileName: string) =>
+  `${exportName} ${createImportName(path.basename(fileName).replace(ext, ''))}`;
